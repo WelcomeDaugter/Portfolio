87ec0b11b150577206eca6bfd4efc9b8b8cb947a
Белевич Ярослав <yaroslav2004b@yandex.ru>
2023-05-05 23:36:27 +0300

    ааа

---
+++ Portfolio/Pages/Achivements.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio.Pages
+{
+    public class AchivementsModel : PageModel
+    {
+        private readonly ILogger<AchivementsModel> _logger;
+
+        public AchivementsModel(ILogger<AchivementsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
